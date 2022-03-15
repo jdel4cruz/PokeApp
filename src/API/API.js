7435bad25731f6fcec3cf785eaf95317c0031f4e
John De La Cruz <john.e.delacruz34@gmail.com>
2022-03-14 19:28:27 -0700

    refactoring of consts to constants.js for API.js completed.

--- src/API/API.js
+++ src/API/API.js
@@ -4,6 +4,11 @@ import {
   pokemonGridQuery,
   pokemonDescriptionQuery,
   typeQuery,
+  weaknessQuery,
+  itemsQuery,
+  pokemonMovesQuery,
+  evoQuery,
+  allAbilitiesQuery,
 } from "./constants";
 
 //Helper Functions
@@ -46,30 +51,14 @@ const apiSettings = {
         ? typeData.pokemon_v2_pokemontype[1].pokemon_v2_type.name
         : "";
 
-    const weaknessQuery = ` fragment typeWeaknessFields on pokemon_v2_typeefficacy {
-      damage_factor
-      pokemon_v2_type {
-        name
-      }
-      pokemonV2TypeByTargetTypeId {
-        name
-      }
-    }
-    query TypeWeaknesses($type1: String = "", $type2: String = "") {
-      weakness1: pokemon_v2_typeefficacy(where: {pokemonV2TypeByTargetTypeId: {name: {_eq: $type1}}}) {
-        ...typeWeaknessFields
-      }
-      weakness2: pokemon_v2_typeefficacy(where: {pokemonV2TypeByTargetTypeId: {name: {_eq: $type2}}}) {
-        ...typeWeaknessFields
-      }
-    }`;
+    const wQuery = weaknessQuery;
 
     const weaknessVars = {
       type1,
       type2,
     };
 
-    const weaknessData = await fetchData(weaknessQuery, weaknessVars);
+    const weaknessData = await fetchData(wQuery, weaknessVars);
 
     const weakness1 = weaknessData.weakness1;
     const weakness2 = weaknessData.weakness2;
@@ -78,41 +67,7 @@ const apiSettings = {
   },
 
   fetchMoves: async (pokemonId) => {
-    const query = `
-    fragment PokemonMoveFields on pokemon_v2_pokemonmove {
-      level
-      pokemon_v2_move {
-        accuracy
-        move_damage_class_id
-        move_effect_chance
-        name
-        power
-        pp
-        priority
-        type_id
-        id
-        pokemon_v2_moveeffect {
-          pokemon_v2_moveeffecteffecttexts {
-            effect
-          }
-        }
-      }
-    }
-    
-    query PokemonLevelMoves($id: Int) {
-      levelMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 1}, version_group_id: {_eq: 18}}, order_by: {level: asc}) {
-        ...PokemonMoveFields
-      }
-      eggMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 2}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
-        ...PokemonMoveFields
-      }
-      tutorMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 3}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
-        ...PokemonMoveFields
-      }
-      machineMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 4}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
-        ...PokemonMoveFields
-      }
-    }`;
+    const query = pokemonMovesQuery;
 
     const vars = {
       id: pokemonId,
@@ -126,50 +81,8 @@ const apiSettings = {
   },
 
   fetchPokemonEvolutions: async (pokemonId) => {
-    const query = `query PokemonEvolution($id: [Int!]) {
-      pokemon_v2_evolutionchain(where: {pokemon_v2_pokemonspecies: {id: {_in: $id}}}) {
-        pokemon_v2_pokemonspecies(order_by: {evolves_from_species_id: asc_nulls_first}) {
-          id
-          name
-          evolves_from_species_id
-          pokemon_v2_pokemonevolutions(limit: 1, order_by: {id: desc}) {
-            evoTrigger:pokemon_v2_evolutiontrigger {
-              name
-            }
-            useItem:pokemon_v2_item {
-              name
-            }
-            location:pokemon_v2_location {
-              name
-            }
-            heldItem:pokemonV2ItemByHeldItemId {
-              name
-            }
-            gender:pokemon_v2_gender {
-              name
-            }
-            learnedMove:pokemon_v2_move {
-              name
-            }
-            tradeWith:pokemonV2PokemonspecyByTradeSpeciesId {
-              name
-            }
-            moveType:pokemon_v2_type {
-              name
-            }
-            affection: min_affection
-            beauty: min_beauty
-            happiness: min_happiness
-            level: min_level
-            needsRain:needs_overworld_rain
-            time:time_of_day
-            atkIsGreater:relative_physical_stats
-            turnUpsideDown:turn_upside_down
-          }
-        }
-      }
-    }
-    `;
+    const query = evoQuery;
+
     const vars = {
       id: pokemonId,
     };
@@ -193,28 +106,7 @@ const apiSettings = {
   },
 
   fetchItems: async () => {
-    const query = `
-    query PokemonItems {
-      items: pokemon_v2_item(order_by: {id: asc}) {
-        name
-        id
-        cost
-        itemEffect: pokemon_v2_itemeffecttexts {
-          effect
-        }
-        attributes: pokemon_v2_itemattributemaps(order_by: {item_attribute_id: desc}) {
-          attribute: pokemon_v2_itemattribute {
-            name
-            id
-          }
-        }
-        rawCategory: pokemon_v2_itemcategory {
-          name
-          id
-        }
-      }
-    }
-      `;
+    const query = itemsQuery;
 
     const data = await fetchData(query);
     const items = data.items;
@@ -269,37 +161,18 @@ const apiSettings = {
     `;
     }
 
-    // console.log(query);
-
     const data = await fetchData(query);
     updateAllMoveText(data);
-    // console.log(data);
 
     return data;
   },
 
   fetchAbilities: async () => {
-    const query = `
-    query AllMoves {
-      abilities: pokemon_v2_ability(order_by: {name: asc}) {
-        id
-        name
-        abilityEffect: pokemon_v2_abilityeffecttexts(where: {language_id: {_eq: 9}}) {
-          effect
-        }
-        pokemonInfo: pokemon_v2_pokemonabilities {
-          pokemon: pokemon_v2_pokemon {
-            name
-          }
-          is_hidden
-        }
-      }
-    }
-    `;
+    const query = allAbilitiesQuery;
 
     const data = await fetchData(query);
-    console.log(data);
     const splitData = splitPokemonAbilties(data.abilities);
+
     return splitData;
   },
 };
--- src/API/constants.js
+++ src/API/constants.js
@@ -81,3 +81,143 @@ export const typeQuery = `query PokemonType($id: Int) {
       }
     }
       }`;
+
+export const weaknessQuery = `fragment typeWeaknessFields on pokemon_v2_typeefficacy {
+    damage_factor
+    pokemon_v2_type {
+      name
+    }
+    pokemonV2TypeByTargetTypeId {
+      name
+    }
+  }
+  query TypeWeaknesses($type1: String = "", $type2: String = "") {
+    weakness1: pokemon_v2_typeefficacy(where: {pokemonV2TypeByTargetTypeId: {name: {_eq: $type1}}}) {
+      ...typeWeaknessFields
+    }
+    weakness2: pokemon_v2_typeefficacy(where: {pokemonV2TypeByTargetTypeId: {name: {_eq: $type2}}}) {
+      ...typeWeaknessFields
+    }
+  }`;
+
+export const itemsQuery = `
+query PokemonItems {
+  items: pokemon_v2_item(order_by: {id: asc}) {
+    name
+    id
+    cost
+    itemEffect: pokemon_v2_itemeffecttexts {
+      effect
+    }
+    attributes: pokemon_v2_itemattributemaps(order_by: {item_attribute_id: desc}) {
+      attribute: pokemon_v2_itemattribute {
+        name
+        id
+      }
+    }
+    rawCategory: pokemon_v2_itemcategory {
+      name
+      id
+    }
+  }
+}
+  `;
+
+export const pokemonMovesQuery = `
+fragment PokemonMoveFields on pokemon_v2_pokemonmove {
+  level
+  pokemon_v2_move {
+    accuracy
+    move_damage_class_id
+    move_effect_chance
+    name
+    power
+    pp
+    priority
+    type_id
+    id
+    pokemon_v2_moveeffect {
+      pokemon_v2_moveeffecteffecttexts {
+        effect
+      }
+    }
+  }
+}
+
+query PokemonLevelMoves($id: Int) {
+  levelMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 1}, version_group_id: {_eq: 18}}, order_by: {level: asc}) {
+    ...PokemonMoveFields
+  }
+  eggMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 2}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
+    ...PokemonMoveFields
+  }
+  tutorMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 3}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
+    ...PokemonMoveFields
+  }
+  machineMoves: pokemon_v2_pokemonmove(where: {pokemon_id: {_eq: $id}, move_learn_method_id: {_eq: 4}, version_group_id: {_eq: 18}}, order_by: {pokemon_v2_move: {name: asc}}) {
+    ...PokemonMoveFields
+  }
+}`;
+
+export const evoQuery = `query PokemonEvolution($id: [Int!]) {
+    pokemon_v2_evolutionchain(where: {pokemon_v2_pokemonspecies: {id: {_in: $id}}}) {
+      pokemon_v2_pokemonspecies(order_by: {evolves_from_species_id: asc_nulls_first}) {
+        id
+        name
+        evolves_from_species_id
+        pokemon_v2_pokemonevolutions(limit: 1, order_by: {id: desc}) {
+          evoTrigger:pokemon_v2_evolutiontrigger {
+            name
+          }
+          useItem:pokemon_v2_item {
+            name
+          }
+          location:pokemon_v2_location {
+            name
+          }
+          heldItem:pokemonV2ItemByHeldItemId {
+            name
+          }
+          gender:pokemon_v2_gender {
+            name
+          }
+          learnedMove:pokemon_v2_move {
+            name
+          }
+          tradeWith:pokemonV2PokemonspecyByTradeSpeciesId {
+            name
+          }
+          moveType:pokemon_v2_type {
+            name
+          }
+          affection: min_affection
+          beauty: min_beauty
+          happiness: min_happiness
+          level: min_level
+          needsRain:needs_overworld_rain
+          time:time_of_day
+          atkIsGreater:relative_physical_stats
+          turnUpsideDown:turn_upside_down
+        }
+      }
+    }
+  }
+  `;
+
+export const allAbilitiesQuery = `
+query AllMoves {
+  abilities: pokemon_v2_ability(order_by: {name: asc}) {
+    id
+    name
+    abilityEffect: pokemon_v2_abilityeffecttexts(where: {language_id: {_eq: 9}}) {
+      effect
+    }
+    pokemonInfo: pokemon_v2_pokemonabilities {
+      pokemon: pokemon_v2_pokemon {
+        name
+      }
+      is_hidden
+    }
+  }
+}
+`;
